638db6c64901227fdece8acb9f9199dc21e76d91
Siddharth <siddharthhooda0013@gmail.com>
2025-07-24 23:28:05 +0530

    fix: update description for clarity and add favicon for improved branding

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "SortedHive - Your trusted URL shortener",
-  description: "Bitlinks helps you shorten your Urls easily",
+  description: "SortedHive helps you shorten your Urls easily and securely.",
 };
 
 export default function RootLayout({ children }) {
