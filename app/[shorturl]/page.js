45630e551f6e52baaeb397d33eed749dd863e781
Siddharth <siddharthhooda0013@gmail.com>
2025-07-24 20:52:12 +0530

    fix: streamline MongoDB connection logic and enhance error message clarity

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,18 +1,22 @@
-import { redirect } from "next/navigation";
-import clientPromise from "@/lib/mongodb";
+import { redirect } from "next/navigation"
+import clientPromise from "@/lib/mongodb"
+
 
 export default async function Page({ params }) {
-  const shorturl = params.shorturl; // ✅ correct - do not await
+    const shorturl = (await params).shorturl
 
-  const client = await clientPromise;
-  const db = client.db("bitlinks");
-  const collection = db.collection("url");
+    const client = await clientPromise;
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
 
-  const doc = await collection.findOne({ shorturl });
+    const doc = await collection.findOne({shorturl: shorturl})
+    console.log(doc)
+    if(doc){
+         redirect(doc.url)
+    }
+    else{
+        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
+    }
 
-  if (doc && doc.url) {
-    redirect(doc.url);
-  } else {
-    redirect(process.env.NEXT_PUBLIC_HOST);
+    return <div>My Post: {url}</div>
   }
-}
